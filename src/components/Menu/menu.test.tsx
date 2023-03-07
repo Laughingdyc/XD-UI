6ed8e7bf73549a11d9b29d0eeb2bdfb85f667649
My Name Is DuYuChen <light_community@qq.com>
2023-03-08 00:54:54 +0800

    feat: 补充点 test

--- src/components/Menu/menu.test.tsx
+++ src/components/Menu/menu.test.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { fireEvent, render, RenderResult, cleanup } from "@testing-library/react";
+import { fireEvent, render, RenderResult, cleanup, waitFor } from "@testing-library/react";
 import Menu, { MenuProps } from "./menu";
 import MenuItem from "./menuItem";
+import SubMenu from "./subMenu";
 
 const testProps: MenuProps = {
     onSelect: jest.fn(),
@@ -24,6 +25,10 @@ const generateMenu = (props: MenuProps) => {
             <MenuItem>
                 default
             </MenuItem>
+            <SubMenu title="dropdown">
+                <MenuItem>dropdown-1</MenuItem>
+                <MenuItem>dropdown-2</MenuItem>
+            </SubMenu>
         </Menu>
     )
 }
@@ -33,9 +38,24 @@ let menuElement    : HTMLElement
 let activeElement  : HTMLElement
 let disabledElement: HTMLElement
 
+const createStyleFile = () => {
+    const cssFile: string = `
+        .XD-submenu{
+            display: none;
+        }
+        .XD-submenu.menu-opened{
+            display: block;
+        }
+    `
+    const style = document.createElement('style')
+    style.innerHTML = cssFile
+    return style
+}
+
 describe('test Menu and MenuItem component', () => {
     beforeEach(() => {
         wrapper         = render(generateMenu(testProps))
+        wrapper.container.append(createStyleFile())
         menuElement     = wrapper.getByTestId('XD-menu')
         activeElement   = wrapper.getByText('active')
         disabledElement = wrapper.getByText('disabled')
@@ -44,7 +64,7 @@ describe('test Menu and MenuItem component', () => {
     it('should render correct Menu and MenuItem base on default props', () => {
         expect(menuElement).toBeInTheDocument()
         expect(menuElement).toHaveClass( ...[ 'XD-menu', 'testClass' ] )
-        expect(menuElement.getElementsByTagName('li').length).toEqual(3)
+        expect(menuElement.querySelectorAll(':scope > li').length).toEqual(4)
         expect(activeElement).toHaveClass( ...[ 'XD-menu-item', 'is-active' ] )
         expect(disabledElement).toHaveClass( ...[ 'XD-menu-item', 'is-disabled' ] )
     })
@@ -64,6 +84,19 @@ describe('test Menu and MenuItem component', () => {
         cleanup()
         const wrapper: RenderResult = render(generateMenu(testVerProps))
         const menuElement = wrapper.getByTestId('XD-menu')
-        expect(menuElement).toHaveClass('menu-vertical')
+        expect(menuElement).toHaveClass( ...[ 'menu-vertical' ] )
+    })
+
+    it('should show dropdown items when hover on subMenu', async () => {
+        expect(wrapper.queryByText('dropdown-1')).not.toBeVisible()
+        const dropdownElement = wrapper.getByText('dropdown')
+        fireEvent.mouseEnter(dropdownElement)
+        await waitFor(() => {
+            expect(wrapper.queryByText('dropdown-1')).toBeVisible()
+        })
+        fireEvent.click(dropdownElement)
+        expect(wrapper.queryByText('dropdown-1')).toBeVisible()
+        fireEvent.click(wrapper.getByText('dropdown-1'))
+        expect(testProps.onSelect).toHaveBeenCalledWith('3-0')
     })
 })
