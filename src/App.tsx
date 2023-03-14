aecd21e120c631d0028305944674e958db7c01fb
My Name Is DuYuChen <light_community@qq.com>
2023-03-15 01:10:56 +0800

    feat: fontAwesomeIcon

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import './App.scss'
 import './styles/index.scss';
 import ButtonExample from './examples/ButtonExample/ButtonExample';
 import MenuExample from './examples/MenuExample/MenuExample';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   return (
     <>
       <ButtonExample></ButtonExample>
       <MenuExample></MenuExample>
+      <FontAwesomeIcon icon={faEnvelope} />
     </>
   );
 }
